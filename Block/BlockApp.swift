ef7c5286c2c72982013a9d8a4e11895f1c7c8e70
ykisana <ykisana@protonmail.com>
2025-11-16 01:46:37 -0700

    init: create project

---
+++ Block/BlockApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlockApp.swift
+//  Block
+//
+//  Created by Yahya Kisana on 2025-11-16.
+//
+
+import SwiftUI
+
+@main
+struct BlockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BlockTests/BlockTests.swift
@@ -0,0 +1,17 @@
+//
+//  BlockTests.swift
+//  BlockTests
+//
+//  Created by Yahya Kisana on 2025-11-16.
+//
+
+import Testing
+@testable import Block
+
+struct BlockTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
