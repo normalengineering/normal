45fb1cc3bfcb9f000727076921bca21b3b61a663
ykisana <ykisana@protonmail.com>
2026-01-23 17:06:31 -0700

    refactor: generalize card view

--- Block/Shared/Components/CardView.swift
+++ Block/Shared/Components/CardView.swift
@@ -1,21 +1,11 @@
 import SwiftUI
 
 struct CardView<Content: View>: View {
-    let primaryText: String
-    let secondaryText: String
     @ViewBuilder let content: Content
 
     var body: some View {
         VStack(alignment: .leading, spacing: 16) {
-            Text(primaryText)
-                .font(.headline)
-            HStack {
-                content
-                Spacer()
-                Text(secondaryText)
-                    .font(.system(size: 16, weight: .bold, design: .rounded))
-                    .foregroundColor(.secondary)
-            }
+            content
         }
         .padding()
         .contentShape(Rectangle())
--- Block/Views/AppSelect/AppGroupListCardView.swift
+++ Block/Views/AppSelect/AppGroupListCardView.swift
@@ -1,50 +1,57 @@
-import SwiftUI
+import FamilyControls
 import ManagedSettings
 import SwiftData
-import FamilyControls
-struct AppGroupListCardView : View {
+import SwiftUI
+
+struct AppGroupListCardView: View {
     @Environment(\.modelContext) private var modelContext
-    
-    let appGroup : AppGroup
+
+    let appGroup: AppGroup
     let displayLimit = 3
     private var tokens: [ApplicationToken] {
-            Array(appGroup.selection.applicationTokens)
-        }
-    
-    var body: some View {
-            CardView(primaryText: appGroup.name, secondaryText: "\(appGroup.selection.applicationTokens.count) apps") {
-                groupIconView
-            }
-            .contextMenu {
-                Button(role: .destructive) {
-                    modelContext.delete(appGroup)
-                } label: {
-                    Label("Delete", systemImage: "trash")
-                }
-            }
-        
+        Array(appGroup.selection.applicationTokens)
     }
-    
-    private var groupIconView: some View {
-            let appsToShow = tokens.prefix(displayLimit)
-            
-        return HStack(spacing: -4 ) {
-                ForEach(appsToShow, id: \.self) { app in
-                    Label(app)
-                        .labelStyle(.iconOnly)
-                }
-                
-                if tokens.count > displayLimit {
-                    moreIndicator
-                }
+
+    var body: some View {
+        CardView() {
+            Text(appGroup.name)
+                .font(.headline)
+            HStack {
+                groupIconView
+                Spacer()
+                Text("\(appGroup.selection.applicationTokens.count) apps")
+                    .font(.system(size: 16, weight: .bold, design: .rounded))
+                    .foregroundColor(.secondary)
             }
         }
-    
+        .contextMenu {
+            Button(role: .destructive) {
+                modelContext.delete(appGroup)
+            } label: {
+                Label("Delete", systemImage: "trash")
+            }
+        }
+    }
+
+    private var groupIconView: some View {
+        let appsToShow = tokens.prefix(displayLimit)
+
+        return HStack(spacing: -4) {
+            ForEach(appsToShow, id: \.self) { app in
+                Label(app)
+                    .labelStyle(.iconOnly)
+            }
+
+            if tokens.count > displayLimit {
+                moreIndicator
+            }
+        }
+    }
+
     private var moreIndicator: some View {
         Text("+\(tokens.count - displayLimit)")
             .font(.system(size: 11, weight: .bold, design: .rounded))
             .foregroundStyle(.secondary)
             .padding(.leading, 4)
-        }
+    }
 }
-
